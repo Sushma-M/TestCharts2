ea2d799610e9112c19ad7b5341033b64ebba146b
root <root@a65be7221041>
2016-09-23 05:26:27 +0000

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,11 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "material",
+  "defaultLanguage" : "en",
+  "displayName" : "TestCharts2",
+  "homePage" : "Main",
+  "name" : "TestCharts2",
+  "platformType" : "WEB",
+  "supportedLanguages" : "en",
+  "type" : "APPLICATION",
+  "version" : "1.0"
+};
